b957bd4351686ebf8c9995468033e92ae91e5883
Jolene Kearse <pharaohnutz@gmail.com>
2022-09-29 11:38:53 -0400

    hex, rgb, hsl working

--- index.js
+++ index.js
@@ -27,31 +27,82 @@ document.querySelector(".theme-icon").addEventListener("click", () => {
 
 // CORE FUNCTIONALITY
 
+// variables
 const form = document.getElementById("form");
 const schemeDisplay = document.getElementById("scheme-display");
 let colorDiv;
 
+// functions
 const addColorDisplay = (arr) => {
   // create color divs
   colorDiv = document.createElement("div");
   colorDiv.classList.add("color-display-divs");
+  colorDiv.style.backgroundColor = ColorsHexCodes;
   colorDiv.style.backgroundColor = arr;
   schemeDisplay.appendChild(colorDiv);
   addColorCode(arr);
 };
 
 const addColorCode = (arr) => {
-  // add text to color divs
-  const hexCode = document.createElement("p");
-  hexCode.innerHTML = `<p>${arr}</p>`;
-  colorDiv.appendChild(hexCode);
+  const colorCode = document.createElement("p");
+  colorCode.innerHTML = `<p>${arr}</p>`;
+  colorDiv.appendChild(colorCode);
 };
+
+// for hsv & cmyk, which can't be used as backgroundColor for colorDiv
+const addNonSupportedColorDisplay = (arr) => {
+  console.log(arr);
+  colorDiv = document.createElement("div");
+  colorDiv.classList.add("color-display-divs");
+  colorDiv.style.backgroundColor = hsvToRgb(arr);
+  schemeDisplay.appendChild(colorDiv);
+  addColorCode(arr);
+};
+
+// strip hsv of extra chars 😡 😡 😡 Need to use regex?!?!
+function stripHsvCode(hsv) {
+
+}
+// convert hsv to rgb
+function hsvToRgb(h, s, v) {
+  let r, g, b;
+
+  const i = Math.floor(h * 6);
+  const f = h * 6 - i;
+  const p = v * (1 - s);
+  const q = v * (1 - f * s);
+  const t = v * (1 - (1 - f) * s);
+
+  switch (i % 6) {
+    case 0:
+      (r = v), (g = t), (b = p);
+      break;
+    case 1:
+      (r = q), (g = v), (b = p);
+      break;
+    case 2:
+      (r = p), (g = v), (b = t);
+      break;
+    case 3:
+      (r = p), (g = q), (b = v);
+      break;
+    case 4:
+      (r = t), (g = p), (b = v);
+      break;
+    case 5:
+      (r = v), (g = p), (b = q);
+      break;
+  }
+  return [r * 255, g * 255, b * 255];
+}
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   // get user input
   const startingColor = document.getElementById("seedColor").value;
   const scheme = document.getElementById("schemeSelect").value;
+  const codeName = document.getElementById("colorCodeName").value;
+  console.log(`codeName: ${codeName}`);
 
   // build the url using user input
   const url = `https://www.thecolorapi.com/scheme?hex=${startingColor.substring(
@@ -64,10 +115,32 @@ form.addEventListener("submit", function (e) {
     .then((data) => {
       // put color data in an array
       const ColorsData = data.colors;
-      console.log(ColorsData);
-      // get all hex codes in an array
-      const ColorsHexCodes = ColorsData.map((color) => color.hex.value);
-      console.log(ColorsHexCodes);
-      ColorsHexCodes.forEach((ele) => addColorDisplay(ele));
+      // get all color codes in an array
+      ColorsHexCodes = ColorsData.map((color) => color.hex.value);
+      const ColorsRgbCodes = ColorsData.map((color) => color.rgb.value);
+      const ColorsHslCodes = ColorsData.map((color) => color.hsl.value);
+      const ColorsHsvCodes = ColorsData.map((color) => color.hsv.value);
+      const ColorsCmykCodes = ColorsData.map((color) => color.cmyk.value);
+
+      // USE SWITCH BEFORE DISPLAYING THE COLORS OR CODES
+      switch (codeName) {
+        case "rgb":
+          ColorsRgbCodes.forEach((ele) => addColorDisplay(ele));
+          break;
+        case "hsl":
+          ColorsHslCodes.forEach((ele) => addColorDisplay(ele));
+          break;
+        case "hsv":
+          ColorsHsvCodes.forEach((ele) => addNonSupportedColorDisplay(ele));
+          break;
+        case "cmyk":
+          ColorsCmykCodes.forEach((ele) => addNonSupportedColorDisplay(ele));
+          break;
+        default:
+          ColorsHexCodes.forEach((ele) => addColorDisplay(ele));
+      }
+
+      // use addColorDisplay on line 34 to show colors to screen
+      // ColorsHexCodes.forEach((ele) => addColorDisplay(ele));
     });
 });
